ea26f098dd4181d1eb8ec21315e7b36d8c29a1e9
theophile <theomolinatti@gmail.com>
2020-03-16 12:34:29 +0100

    Fonction arete, sommet, + fusion des deux faites modif possible pour opti

--- theophile.py
+++ theophile.py
@@ -11,11 +11,26 @@ class Graphe:
         self.sommets = []
         self.aretes = []
         self.fichier = fichier
-    
-    def init_aretes(self):
-        f = open(self.fichier,"r")
 
-        for chaine in f :
+    def init_sommets(self):
+        file = open(self.fichier,"r")
+
+        for chaine in file :
+            if chaine[0:1] == "V" :
+                chaine = chaine.replace("\n","")
+                chaine = chaine.replace("V ","")
+            
+                #On sépare la chaine grâce aux espaces
+                infoSommet = chaine.split(" ")
+                #On ajoute [numSommet, numLigne, nom] à la liste des sommets
+                (self.sommets).append(infoSommet)
+                print (chaine)
+        file.close()
+
+    def init_aretes(self):
+        file = open(self.fichier,"r")
+
+        for chaine in file :
             if chaine[0:1] == "E" :
                 chaine = chaine.replace("\n","")
                 chaine = chaine.replace("E ","")
@@ -25,14 +40,40 @@ class Graphe:
                 #On ajoute [s1, s2, poid] à la liste des aretes
                 (self.aretes).append(arete)
                 print (chaine)
-        f.close()
+        file.close()
 
-    #def init_sommets(self):
+    #fusion des deux fonctions du dessus pour eviter de lire plusieur fois le fichiers car lourd en execution
+    #Possible de fusionner les deux if car beaucoup d'action en commun
+    def init_graph(self):
+        
+        file = open(self.fichier,"r")
 
-    #sommet1 = départ   sommet2 = arrivée
-    #def dijk(self,sommet1,sommet2):
+        for chaine in file :
+            #Permet d'avoir les informations sur un sommet
+            if chaine[0:1] == "V" :
+                chaine = chaine.replace("\n","")
+                chaine = chaine.replace("V ","")
+            
+                #On sépare la chaine grâce aux espaces
+                infoSommet = chaine.split(" ")
+                
+                #On ajoute [numSommet, numLigne, nom] à la liste des sommets
+                (self.sommets).append(infoSommet)
+                print (chaine)
+            
+            #Permet d'avoir les informations sur les aretes
+            if chaine[0:1] == "E" :
+                chaine = chaine.replace("\n","")
+                chaine = chaine.replace("E ","")
+            
+                #On sépare la chaine grâce aux espaces
+                arete = chaine.split(" ")
+                
+                #On ajoute [s1, s2, poid] à la liste des aretes
+                (self.aretes).append(arete)
+                print (chaine)
 
-
+        file.close()
 
 G = Graphe("metro.txt")
 G.init_aretes()
