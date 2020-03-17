b50b3d17c721d3627de1f8ad2e25e21fa8f0ab09
Vince780 <vincent_xavier780@yahoo.fr>
2020-03-17 14:49:17 +0100

    Prise en compte des deux différents formats

--- vincent.py
+++ vincent.py
@@ -14,34 +14,59 @@ class Graphe:
     
     def init_aretes(self):
         f = open(self.fichier,"r")
+        #chaine == "" : fin du fichier
         while(1):
             chaine = f.readline()
-            chaine = chaine.replace("\n","")
-            chaine = chaine.replace("E ","")
-            #On stoppe à la fin du fichier
-            if (chaine == ""):
+            if(chaine == ""):
                 break
-            #On sépare la chaine grâce aux espaces
-            arete = chaine.split(" ")
-            #On ajoute [s1, s2, poid] à la liste des aretes
-            (self.aretes).append(arete)
-            print chaine
+            if(chaine[0] == 'E'):
+                chaine = chaine.replace("\n","")
+                chaine = chaine.replace("E ","")
+                #On sépare la chaine grâce aux espaces
+                arete = chaine.split(" ")
+                #On ajoute [s1, s2, poid] à la liste des aretes
+                (self.aretes).append(arete)
+                #print chaine
         f.close()
 
-    #def init_sommets(self):
+    def init_sommets(self):
+        f = open(self.fichier,"r")
+        #chaine == "" : fin du fichier
+        while(1):
+            chaine = f.readline()
+            if(chaine == ""):
+                break
+            if(chaine[0] == 'V'):
+                chaine = chaine.replace("\n","")
+                chaine = chaine.replace("V ","")
+                #On sépare la chaine grâce aux espaces
+                sommet = chaine.split(" ")
+                (self.sommets).append(sommet)
+        f.close()
 
     #sommet1 = départ   sommet2 = arrivée
-    #def dijk(self,sommet1,sommet2):
+   # def dijk(self,sommet1,sommet2):
+        #On vérifie que les deux sommets existent
+   #     if(sommet1 not in self.sommets):
+   #         print "Votre point de départ n'est pas bon..."
+   #         return
+   #     if(sommet2 not in self.sommets):
+   #         print "Votre point d'arrivé n'est pas bon"
+   #         return
+   #     T = [sommet1]
+   #     distance_sommet = 0
 
 
 
 G = Graphe("metro_test.txt")
 G.init_aretes()
+G.init_sommets()
 print G.aretes
-
-#Exemple pour lire un poid :
 print ""
-print "Voici les 2 premier poids : "
-print G.aretes[0][2]
-print G.aretes[1][2]
-#etc,etc...
+print G.sommets
+print ""
+
+sommet1 = raw_input("Où êtes-vous ?  ")
+sommet2 = raw_input("Où voulez-vous aller ?  ")
+
+#G.dijk(sommet1,sommet2)
